50cc0914d87aae204eeb1d56c68784d7970ea903
Arthur Schreiber <schreiber.arthur@gmail.com>
2021-07-23 16:25:01 +0000

    refactor: export `ConnectionOptions` interface

--- src/connection.ts
+++ src/connection.ts
@@ -489,7 +489,7 @@ interface AuthenticationOptions {
   options?: any;
 }
 
-interface ConnectionOptions {
+export interface ConnectionOptions {
   /**
    * A boolean determining whether to rollback a transaction automatically if any error is encountered
    * during the given transaction's execution. This sets the value for `SET XACT_ABORT` during the
